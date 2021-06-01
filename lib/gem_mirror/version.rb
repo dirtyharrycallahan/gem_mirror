1c8a87598b6dd1708fd26fe2c79d9d9ce3c69c72
Patrick <pmc@patrickcallahan.com>
2021-06-01 17:14:40 -0400

    Update version.rb
    white space

--- lib/gem_mirror/version.rb
+++ lib/gem_mirror/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module GemMirror
-    # The current gem version as a string
-    VERSION = "0.1.0.pre"
+  # The current gem version as a string
+  VERSION = "0.1.0.pre"
 end
