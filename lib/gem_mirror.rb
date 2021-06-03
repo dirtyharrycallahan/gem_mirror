528bcac0750be208956a9225e8b3fb71f5d34979
Patrick Callahan <pmc@patrickcallahan.com>
2021-06-03 08:53:54 -0400

    requires: simplify require by globbing

--- lib/gem_mirror.rb
+++ lib/gem_mirror.rb
@@ -1,31 +1,19 @@
 # frozen_string_literal: true
 
+require "confstruct"
+require "httpclient"
+require "digest/sha2"
+require "fileutils"
+require "logger"
 require "rubygems"
 require "rubygems/user_interaction"
 require "rubygems/indexer"
-require "fileutils"
-require "digest/sha2"
-require "confstruct"
+require "stringio"
 require "slop"
 require "zlib"
-require "httpclient"
-require "logger"
-require "stringio"
 
 $LOAD_PATH.unshift(File.expand_path(__dir__)) unless $LOAD_PATH.include?(File.expand_path(__dir__))
 
-require_relative "gem_mirror/version"
-require_relative "gem_mirror/configuration"
-require_relative "gem_mirror/gem"
-require_relative "gem_mirror/source"
-require_relative "gem_mirror/mirror_directory"
-require_relative "gem_mirror/mirror_file"
-require_relative "gem_mirror/versions_file"
-require_relative "gem_mirror/versions_fetcher"
-require_relative "gem_mirror/gems_fetcher"
-
-require_relative "gem_mirror/cli"
-require_relative "gem_mirror/cli/checksum"
-require_relative "gem_mirror/cli/index"
-require_relative "gem_mirror/cli/init"
-require_relative "gem_mirror/cli/update"
+Dir["./lib/**/*.rb"].sort.each do |file|
+  require file
+end
